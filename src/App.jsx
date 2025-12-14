e43b43ee732904d3a1ae7b2e5d6ae5bb7a5180bc
Harsh Vankar <cursedpower4@gmail.com>
2025-12-14 21:04:54 +0530

    App.jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,13 @@ import { useEffect, useState } from "react"
 function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     setIsMobile(window.innerWidth < 768);
-  //   };
-  //   window.addEventListener("resize", handleResize);
-  //   return () => window.removeEventListener("resize", handleResize);
-  // }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <Router>
